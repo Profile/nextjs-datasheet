50b14048d6b0d851a86c8ecdf18f84b8c8c6d267
Aslan <>
2021-02-27 20:07:11 +0400

    Add initial sheets page

---
+++ pages/sheets/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+export default function Sheets() {
+  return (
+    <div>
+      <Head>
+        <title>Sheets - Employees</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+          {/* TODO: Search functionality*/}
+          {/* TODO: Some basic validations (date, phone number validation etc.)*/}
+          {/* TODO: Pagination*/}
+      </main>
+    </div>
+  )
+}
