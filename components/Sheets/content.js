56f312181781301af5731169c5b6d38b8def1450
Aslan <>
2021-02-27 22:40:48 +0400

    Add basic editable cell

--- components/Sheets/content.js
+++ components/Sheets/content.js
@@ -1,21 +1,36 @@
-import styles from "./Sheets.module.css";
+import styles from './Sheets.module.css';
 
-export function SheetContent({rows, cells}) {
+export function SheetContent({ rows, cells, editableCell, setEditableCell }) {
+    const generateCustomKey = (main, child) => {
+        return `${main}.${child}`;
+    };
+
     return (
         <div className={styles.tableContent}>
-            {
-                rows.map((row, rowIndex) => (
-                    <div className={styles.tableContentRows} key={rowIndex}>
-                        {cells.map(cell => (
-                            <div className={styles.tableContentCell} key={cell}>
-                                <div className={styles.tableContentCellValue}>
+            {rows.map((row, rowIndex) => (
+                <div className={styles.tableContentRows} key={rowIndex}>
+                    {cells.map((cell) => (
+                        <div className={styles.tableContentCell} key={cell}>
+                            {editableCell === generateCustomKey(cell, rowIndex) ? (
+                                <input
+                                    className={styles.tableContentCellInput}
+                                    type="text"
+                                    value={row[cell]}
+                                    autoFocus={true}
+                                />
+                            ) : (
+                                <div
+                                    className={styles.tableContentCellValue}
+                                    onDoubleClick={() =>
+                                        setEditableCell(generateCustomKey(cell, rowIndex))
+                                    }>
                                     {row[cell]}
                                 </div>
-                            </div>
-                        ))}
-                    </div>
-                ))
-            }
+                            )}
+                        </div>
+                    ))}
+                </div>
+            ))}
         </div>
-    )
+    );
 }
--- pages/sheets/index.js
+++ pages/sheets/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head';
+import { useState } from 'react';
 
-import {SheetHeader} from "components/Sheets/header";
-import {SheetContent} from "components/Sheets/content";
+import { SheetHeader } from 'components/Sheets/header';
+import { SheetContent } from 'components/Sheets/content';
 
-import styles from 'styles/Sheets.module.css'
+import styles from 'styles/Sheets.module.css';
 
 const employee = () => ({
     id: 1,
@@ -13,27 +14,36 @@ const employee = () => ({
     position: 'CASHIER',
     phone: '+994000000000'
 });
-const employees = Array.from({length: 5}).fill(employee());
+const employees = Array.from({ length: 5 }).fill(employee());
 
 const headerCells = Object.keys(employee());
 
 export default function Sheets() {
-  return (
-    <div>
-      <Head>
-        <title>Sheets - Employees</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    const [editableCell, setEditableCell] = useState(null);
 
-      <main>
-          <div className={styles.container}>
-              {<SheetHeader cells={headerCells} />}
-              <div> {/* TODO: Search functionality*/} </div>
-              {<SheetContent rows={employees} cells={headerCells} />}
-          </div>
-          {/* TODO: Some basic validations (date, phone number validation etc.)*/}
-          {/* TODO: Pagination*/}
-      </main>
-    </div>
-  )
+    return (
+        <div>
+            <Head>
+                <title>Sheets - Employees</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <div className={styles.container}>
+                    {<SheetHeader cells={headerCells} />}
+                    <div> {/* TODO: Search functionality*/} </div>
+                    {
+                        <SheetContent
+                            editableCell={editableCell}
+                            setEditableCell={setEditableCell}
+                            rows={employees}
+                            cells={headerCells}
+                        />
+                    }
+                </div>
+                {/* TODO: Some basic validations (date, phone number validation etc.)*/}
+                {/* TODO: Pagination*/}
+            </main>
+        </div>
+    );
 }
