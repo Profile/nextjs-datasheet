a2d267e3654918fe7e6ca75614d4079358d88d27
Aslan <>
2021-02-28 13:35:12 +0400

    Add filterable condition

---
+++ components/Sheets/Filter.js
@@ -0,0 +1,24 @@
+import styles from './Sheets.module.css';
+
+export function SheetFilter({ cells, filterValues, onChange }) {
+    const whiteListInputType = ['text', 'date', 'checkbox'];
+
+    return (
+        <div className={styles.tableHeader}>
+            <div className={styles.tableHeaderRows}>
+                {cells.map((cell) => (
+                    <div className={styles.tableHeaderCell} key={cell.name}>
+                        {cell.filterable && (
+                            <input
+                                defaultValue={filterValues[cell.key]}
+                                className={styles.tableFilterCellInput}
+                                type={whiteListInputType.includes(cell.type) && cell.type}
+                                onChange={({ target }) => onChange({ [cell.key]: target.value })}
+                            />
+                        )}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
