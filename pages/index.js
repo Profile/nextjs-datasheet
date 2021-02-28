887814843665c355a12647b0f67feffb890be419
Aslan <>
2021-03-01 01:20:33 +0400

    Redirect home page to sheets list

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { useRouter } from 'next/router'
+import { useEffect } from 'react';
 
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push('/sheets/1?currentPage=1', undefined, {})
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
