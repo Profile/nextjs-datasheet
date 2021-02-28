1c105612b618aff470f090e77425dda93f7c11c2
Aslan <>
2021-03-01 00:33:26 +0400

    Fix message: Return all messages

--- utils/validate.js
+++ utils/validate.js
@@ -1,15 +1,21 @@
 const validate = (key, rest, validationSchema) => {
     const validate = validationSchema[key];
-    if(!validate) return null;
+    if (!validate) return null;
+
+    const errors = { ...rest.error };
 
     for (const fn of Object.values(validate)) {
         const message = fn(rest[key]);
 
-        if(message) {
-            return message
+        if (message) {
+            errors[key] = message;
+            break;
         }
+
+        errors[key] = null;
     }
 
-}
+    return errors;
+};
 
 export default validate;
